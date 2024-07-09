e44c0d65cdca3ef1769b23b9e65710d770fcd938
Giovanna Macedo <giovannamccds@gmail.com>
2024-07-09 10:05:48 -0300

    session funcionandogit add .

--- Functions/Includes/header.php
+++ Functions/Includes/header.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="stylesheet" href="../Style/header.css">
+    <link rel="stylesheet" href="/Projetos/SiteAddItems2/Style/header.css">
 
     <title>Site add itens</title>
 </head>
@@ -32,10 +32,36 @@
                 <li class="nav-item">
                 <a class="nav-link" href="/Projetos/SiteAddItems2/Paginas/listarItems.php">Editar</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="/Projetos/SiteAddItems2/Functions/php/sairFuncao.php">Sair</a>
-                </li>
             </ul>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Sair
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Sair</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Deseja sair?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                    <button type="button" class="btn btn-primary" ><a href="/Projetos/SiteAddItems2/Functions/php/sairFuncao.php" id="a">Sair</a></button>
+                </div>
+                </div>
+                <style>
+                    /* o style esta aqui pois estava bugando */
+                    #a{
+                        color: aliceblue;
+                    }
+                </style>
+            </div>
+            </div>
         </div>
     </nav>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
